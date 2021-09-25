f3603cf54cb650781bfe708394ddbed4fbe658f5
Riley Stewart <me@rileystew.art>
2021-09-25 15:17:53 -0700

    overoptimised

--- src/formula.rs
+++ src/formula.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 use std::fmt::Debug;
 use std::hash::Hash;
+use parking_lot::Mutex;
 
 use bevy::core::AsBytes;
 
@@ -61,16 +62,18 @@ pub struct Formula<T> where T: FactorSubject {
     pub inputs: Vec<FormulaInput<T>>,
     pub ops: Vec<FormulaOp<T>>,
     pub cached: f32,
+    pub dirty: bool,
 }
 
 
 
 pub struct FormulaSystem<T> where T: FactorSubject {
     factors: HashMap<T, Factors>,
-    formulae: Vec<Formula<T>>,
+    formulae: Arc<Mutex<Vec<Formula<T>>>>,
     input_map: HashMap<FormulaInput<T>, Vec<FormulaId>>,
 }
 
+// TODO: don't propogate onto end nodes
 impl<T> FormulaSystem<T> where T: FactorSubject {
     pub fn add_factor(&mut self, entity: &T, ftype: FactorType, amount: f32) -> f32 {
         if !self.factors.contains_key(entity) {
@@ -93,18 +96,31 @@ impl<T> FormulaSystem<T> where T: FactorSubject {
         }
         // a formula or factor changed
         for &formula_id in self.get_formulae(input).iter() {
-            // look at all the formulas to which is is an input and recalc
-            let before = self.formula_value(formula_id);
-            self.calc_formula(formula_id);
-            let after = self.formula_value(formula_id);
-            if before != after {
-                self.propogate_changes(FormulaInput::Formula(formula_id));
+            // look at all the formulae that factor in to other formulae to which is is an input and recalc
+            if self.input_map.get(&FormulaInput::Formula(formula_id)).map(|v| v.len()).unwrap_or(0) > 0 {
+                let before = self.formula_value(formula_id);
+                self.calc_formula(formula_id);
+                let after = self.formula_value(formula_id);
+                if before != after {
+                    self.propogate_changes(FormulaInput::Formula(formula_id));
+                }
+            } else {
+                self.dirty_formula(formula_id);
             }
         }
     }
 
-    fn formula_value(&self, formula: FormulaId) -> f32 {
-        self.formulae.get(formula.0).map(|f| f.cached).unwrap_or(0.0)
+    fn formula_value(&self, formula_id: FormulaId) -> f32 {
+        let mut formulae = self.formulae.lock();
+        if let Some(formula) = formulae.get_mut(formula_id.0) {
+            if !formula.dirty {
+                formula.cached
+            } else {
+                self.calc_formula_base(formula)
+            }
+        } else {
+            0.0
+        }
     }
 
     fn fetch_inputs(&self, inputs: &Vec<FormulaInput<T>>) -> HashMap<FormulaInput<T>, f32> {
@@ -121,22 +137,40 @@ impl<T> FormulaSystem<T> where T: FactorSubject {
         input_values
     }
 
-    fn calc_formula(&mut self, formula_id: FormulaId) -> f32 {
-        let formula = &self.formulae[formula_id.0];
+    fn dirty_formula(&self, formula_id: FormulaId) {
+        let mut formulae = self.formulae.lock();
+        if let Some(mut formula) = formulae.get_mut(formula_id.0) {
+            formula.dirty = true;
+        }
+    }
+
+    fn calc_formula(&self, formula_id: FormulaId) -> f32 {
+        let mut formulae = self.formulae.lock();
+        if let Some(formula) = formulae.get_mut(formula_id.0) {
+            self.calc_formula_base(formula)
+        } else {
+            0.0
+        }
+    }
+
+    fn calc_formula_base(&self, formula: &mut Formula<T>) -> f32 {
         let inputs = self.fetch_inputs(&formula.inputs);
-        0.0
+        formula.cached = 0.0;
+        formula.dirty = false;
+        formula.cached
     }
 
     fn add_formula_input(&mut self, formula: FormulaId, input: FormulaInput<T>) {
         self.input_map.entry(input).or_default().push(formula);
     }
 
-    pub fn add_formula(&mut self, formula: Formula<T>) -> FormulaId {
-        let idx = self.formulae.len();
+    pub fn add_formula(&self, formula: Formula<T>) -> FormulaId {
+        let mut formulae = self.formulae.lock();
+        let idx = formulae.len();
         let formula_id = FormulaId(idx);
         for input in formula.inputs.iter() {
         }
-        self.formulae.push(formula);
+        formulae.push(formula);
         formula_id
     }
 }
